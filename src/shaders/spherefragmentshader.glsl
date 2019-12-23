9fb32039ca6c726a97f1472572d02e78e1260771
Yuchen Wong <wyc8094@gmail.com>
2019-12-23 15:18:16 +0800

    resolve_comment

--- src/shaders/spherefragmentshader.glsl
+++ src/shaders/spherefragmentshader.glsl
@@ -35,9 +35,9 @@ vec3 getSphereColor(vec3 point) {
 }
 
 void main() {
-  gl_FragColor = vec4(getSphereColor(position), 0.5);
+  gl_FragColor = vec4(getSphereColor(position), 1.0);
   vec4 info = texture2D(water, position.xz * 0.5 + 0.5);
   if (position.y < info.r) {
-    gl_FragColor.rgb *= underwaterColor;
+    gl_FragColor.rgb *= underwaterColor * 1.2;
   }
 }
